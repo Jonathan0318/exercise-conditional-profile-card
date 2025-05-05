f553a055d4a94ac7b1f16b5aa93d6821d1cf06bd
Jonathan0318 <jonathan0318@icloud.com>
2025-05-05 12:12:58 +0200

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -48,10 +48,14 @@ function render(variables = {}) {
           <h2>${role}</h2>
           <h3>${location}</h3>
            <ul class="${socialPositionClass}">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+            <li><a href="https://twitter.com/${variables.twitter ||
+              ""}"><i class="fab fa-twitter"></i></a></li>
+            <li><a href="https://github.com/${variables.github ||
+              ""}"><i class="fab fa-github"></i></a></li>
+            <li><a href="https://linkedin.com/in/${variables.linkedin ||
+              ""}"><i class="fab fa-linkedin"></i></a></li>
+            <li><a href="https://instagram.com/${variables.instagram ||
+              ""}"><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>`;
 }
