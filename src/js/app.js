db2afb46bb889ce8b8ece1ab87b67d9eb0143412
Jonathan0318 <jonathan0318@icloud.com>
2025-05-05 09:54:31 +0200

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -23,27 +23,50 @@ import "../style/index.css";
     }
  */
 function render(variables = {}) {
-  console.log("These are the current variables: ", variables); // print on the console
-  // here we ask the logical questions to make decisions on how to build the html
-  // if includeCover==false then we reset the cover code without the <img> tag to make the cover transparent.
-  let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
-  if (variables.includeCover == false) cover = "<div class='cover'></div>";
+  console.log("These are the current variables: ", variables);
 
-  // reset the website body with the new html output
+  let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
+  if (variables.includeCover === false) cover = "<div class='cover'></div>";
+
+  let fullName = `${variables.name || "Lucy"} ${variables.lastName ||
+    "Boilett"}`;
+  let role = variables.role || "Web Developer";
+  let location = `${variables.city || "Miami"}, ${variables.country || "USA"}`;
+
+  let avatar = variables.avatarURL || "https://via.placeholder.com/150";
+
+  let position =
+    variables.socialMediaPosition === "left"
+      ? "position-left"
+      : "position-right";
+
+  let twitter = variables.twitter
+    ? `<li><a href="https://twitter.com/${variables.twitter}"><i class="fab fa-twitter"></i></a></li>`
+    : "";
+  let github = variables.github
+    ? `<li><a href="https://github.com/${variables.github}"><i class="fab fa-github"></i></a></li>`
+    : "";
+  let linkedin = variables.linkedin
+    ? `<li><a href="https://linkedin.com/in/${variables.linkedin}"><i class="fab fa-linkedin"></i></a></li>`
+    : "";
+  let instagram = variables.instagram
+    ? `<li><a href="https://instagram.com/${variables.instagram}"><i class="fab fa-instagram"></i></a></li>`
+    : "";
+
+  // Reset the website body with the new HTML output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
-            ${cover}
-          <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+          ${cover}
+          <img src="${avatar}" class="photo" />
+          <h1>${fullName}</h1>
+          <h2>${role}</h2>
+          <h3>${location}</h3>
+          <ul class="${position}">
+            ${twitter}
+            ${github}
+            ${linkedin}
+            ${instagram}
           </ul>
-        </div>
-    `;
+        </div>`;
 }
 
 /**
